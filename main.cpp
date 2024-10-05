86e7b2f3643755c158e7081f7f81ee5bec04b521
Aria Kafie <aria.kafie@verizon.net>
2024-10-05 13:16:22 -0700

    thing

--- main.cpp
+++ main.cpp
@@ -1,95 +1,84 @@
 
 #include <chrono>
+#include <fstream>
 #include <iostream>
 #include <sstream>
 
 #include "bitboard.h"
-#include <fstream>
 #include "movegen.h"
 #include "position.h"
 #include "types.h"
 #include "uci.h"
 
-static uint64_t nodes;
-
 template<Color SideToMove>
-void search(int depth)
+uint64_t PerfT(int depth)
 {
     if (depth == 0)
-    {
-        nodes++;
-        return;
-    }
+        return 1;
 
     MoveList<SideToMove> moves;
 
     if (depth == 1)
-    {
-        nodes += moves.size();
-        return;
-    }
+        return moves.size();
 
+    uint64_t nodes = 0;
+    
     for (Move m : moves)
     {
         do_move<SideToMove>(m);
-        search<!SideToMove>(depth -1);
+        nodes += PerfT<!SideToMove>(depth -1);
         undo_move<SideToMove>(m);
     }
+
+    return nodes;
 }
 
 template<Color SideToMove>
-void PerfT(int depth)
+void debug(std::istringstream& is)
 {
+    int   depth;
+    is >> depth;
+    
     uint64_t elapsed = 0, total_nodes = 0;
 
     MoveList<SideToMove> moves;
 
     for (Move m : moves)
     {
-        nodes = 0;
-
-        auto start = std::chrono::steady_clock::now();
+        uint64_t nodes = 0;
 
         do_move<SideToMove>(m);
-        search<!SideToMove>(depth - 1);
+        nodes = PerfT<!SideToMove>(depth - 1);
         undo_move<SideToMove>(m);
 
-        auto end = std::chrono::steady_clock::now();
-
-        elapsed += std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
         total_nodes += nodes;
 
         std::cout << move_to_uci(m) << ": " << nodes << std::endl;
     }
 
-    std::cout << "\nnodes searched: " << total_nodes << "\nin " << (elapsed / 1000) << " ms\n" << std::endl;
-}
-
-void perft(std::istringstream& is)
-{
-    int depth;
-    is >> depth;
-    
-    if (Position::white_to_move()) PerfT<WHITE>(depth);
-    else                           PerfT<BLACK>(depth);
+    std::cout << "\nnodes searched: " << total_nodes << "\n" << std::endl;
 }
 
 void position(std::istringstream& is)
 {
-    std::string fen = "", token;
+    std::string token, fen = "";
 
     is >> token;
 
-    while (is >> token)
-        fen += token + " ";
+    if (token == "startpos")
+        fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    else
+        while (is >> token)
+            fen += token + " ";
 
     Position::set(fen);
 }
 
-void debug()
+void test()
 {
     std::string line, token;
     std::ifstream in("perft_suite.txt");
+    uint64_t expected;
 
     while (std::getline(in, line))
     {
@@ -101,18 +90,14 @@ void debug()
 
         Position::set(fen);
         
-        while (is >> token)
+        while (is >> token >> expected)
         {
             int depth = std::stoi(token.substr(2));
-            uint64_t expected;
-            is >> expected;
 
             std::cout << "PerfT " << depth << " " << fen << std::endl;
 
-            nodes = 0;
-            if (Position::white_to_move()) search<WHITE>(depth);
-            else                           search<BLACK>(depth);
-
+            uint64_t nodes = Position::white_to_move() ? PerfT<WHITE>(depth)
+                                                       : PerfT<BLACK>(depth);
             if (nodes != expected)
                 std::cout << "ERROR" << std::endl;
 
@@ -138,9 +123,14 @@ int main()
         std::istringstream is(cmd);
         is >> token;
 
-        if (token == "perft")    perft(is);
+        if (token == "perft")
+        {
+            if (Position::white_to_move()) debug<WHITE>(is);
+            else                           debug<BLACK>(is);
+        }
+        
         if (token == "position") position(is);
-        if (token == "debug")    debug();
+        if (token == "debug")    test();
         if (token == "d")        std::cout << Position::to_string() << std::endl;
         
     } while (cmd != "quit");
