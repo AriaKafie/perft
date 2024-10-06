bd588f64f286b4dcf28f2113f6d958c16cd6dc90
Aria Kafie <aria.kafie@verizon.net>
2024-10-06 00:06:14 -0700

    update

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -5,8 +5,8 @@
 
 void init_magics();
 
-Bitboard sliding_attacks(PieceType pt, Square sq, Bitboard occupied) {
-
+static Bitboard attacks_bb(PieceType pt, Square sq, Bitboard occupied)
+{
     Direction rook_directions[4] = { NORTH, EAST, SOUTH, WEST };
     Direction bishop_directions[4] = { NORTH_EAST, SOUTH_EAST, SOUTH_WEST, NORTH_WEST };
 
@@ -18,8 +18,8 @@ Bitboard sliding_attacks(PieceType pt, Square sq, Bitboard occupied) {
     return atk;
 }
 
-void Bitboards::init() {
-
+void Bitboards::init()
+{
     for (Square s1 = H1; s1 <= A8; s1++)
     {
         FileBB[s1] = FILE_H << (s1 % 8);
@@ -38,24 +38,24 @@ void Bitboards::init() {
 
         for (Square s = H1; s <= A8; s++)
             for (Bitboard occupied = 0, i = 0; i < 1 << popcount(masks[s]); occupied = generate_occupancy(masks[s], ++i))
-                *table_ptr++ = sliding_attacks(pt, s, occupied ^ (sliding_attacks(pt, s, occupied) & occupied));
+                *table_ptr++ = attacks_bb(pt, s, occupied ^ (attacks_bb(pt, s, occupied) & occupied));
     }
-
-#define mdiag(s) (square_bb(s) | bishop_attacks(s, 0) & (mask(s, NORTH_WEST) | mask(s, SOUTH_EAST)))
-#define adiag(s) (square_bb(s) | bishop_attacks(s, 0) & (mask(s, NORTH_EAST) | mask(s, SOUTH_WEST)))
-
-#define md(a, b) (rank_distance(a, b) + file_distance(a, b))
 
     for (Square s1 = H1; s1 <= A8; s1++)
     {
-        MainDiag[s1] = mdiag(s1);
-        AntiDiag[s1] = adiag(s1);
-            
-        CenterDistance[s1] = std::min({ md(s1, E4), md(s1, E5), md(s1, D4), md(s1, D5) });
+        MainDiag[s1] = bishop_attacks(s1, 0) & (mask(s1, NORTH_WEST) | mask(s1, SOUTH_EAST)) | square_bb(s1);
+        AntiDiag[s1] = bishop_attacks(s1, 0) & (mask(s1, NORTH_EAST) | mask(s1, SOUTH_WEST)) | square_bb(s1);
+        
+        CenterDistance[s1] = std::min({ rank_distance(s1, E4) + file_distance(s1, E4),
+                                        rank_distance(s1, E5) + file_distance(s1, E5),
+                                        rank_distance(s1, D4) + file_distance(s1, D4),
+                                        rank_distance(s1, D5) + file_distance(s1, D5) });
 
         for (Square s2 = H1; s2 <= A8; s2++)
-            AlignMask[s1][s2] = mdiag(s1) & mdiag(s2) | adiag(s1) & adiag(s2) | rank_bb(s1) & rank_bb(s2) | file_bb(s1) & file_bb(s2);
-
+            for (PieceType pt : { BISHOP, ROOK })
+                if (attacks_bb(pt, s1, 0) & square_bb(s2))
+                    AlignMask[s1][s2] = attacks_bb(pt, s1, 0) & attacks_bb(pt, s2, 0) | square_bb(s1, s2);
+                    
         for (Square ksq = s1, checker = H1; checker <= A8; checker++)
         {
             for (Direction d : { NORTH, NORTH_EAST, EAST, SOUTH_EAST, SOUTH, SOUTH_WEST, WEST, NORTH_WEST })
@@ -81,11 +81,7 @@ void Bitboards::init() {
         PawnAttacks[WHITE][s1] = pawn_attacks<WHITE>(square_bb(s1));
         PawnAttacks[BLACK][s1] = pawn_attacks<BLACK>(square_bb(s1));
     }
-
-#undef md
-#undef adiag
-#undef mdiag
-
+    
     uint8_t clearK = 0b0111;
     uint8_t clearQ = 0b1011;
     uint8_t cleark = 0b1101;
@@ -128,10 +124,10 @@ void init_magics() {
         for (Square s = H1; s <= A8; s++)
         {
             base[s] = table_ptr - pext_table;
-            mask[s] = sliding_attacks(pt, s, 0) & ~((FILE_A | FILE_H) & ~file_bb(s) | (RANK_1 | RANK_8) & ~rank_bb(s));
+            mask[s] = attacks_bb(pt, s, 0) & ~((FILE_A | FILE_H) & ~file_bb(s) | (RANK_1 | RANK_8) & ~rank_bb(s));
 
             for (int i = 0; i < 1 << popcount(mask[s]); i++)
-                *table_ptr++ = sliding_attacks(pt, s, generate_occupancy(mask[s], i));
+                *table_ptr++ = attacks_bb(pt, s, generate_occupancy(mask[s], i));
         }
     }
 }
